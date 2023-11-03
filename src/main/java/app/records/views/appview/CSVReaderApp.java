e14fb4371efb2c1cf1a3d3fb54833f4b3e38df80
amukhamedov1 <mukhamedov@trinext.de>
2023-11-03 10:20:06 +0100

    fix(promptBuilder GRP)

--- src/main/java/app/records/views/appview/CSVReaderApp.java
+++ src/main/java/app/records/views/appview/CSVReaderApp.java
@@ -10,17 +10,16 @@ import java.io.*;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.util.*;
-import java.util.Timer;
 import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicInteger;
 
+@SuppressWarnings("RedundantSuppression")
 public class CSVReaderApp {
     private static final List<String> responses = new ArrayList<>();
     private static final Path DOCS_PATH = Path.of("data" + File.separator + "docs" + File.separator + "id_name.csv");
     private static final Path GRPS_PATH = Path.of("data" + File.separator + "docs" + File.separator + "grps.csv");
     private static final String instruction = """
             erstelle aus den übergebenen dokumentnamen eine liste an gruppen.
-            die dokumentnamen sollen semantisch in diese gruppen passen.
+            die dokumentnamen sollen semantisch in die jeweilige gruppen passen.
             antworte mit einer html liste.
             <head></head>, <title></title> und <h1></h1>.
             antworte nicht mit einer textuellen erklärung.
@@ -29,25 +28,14 @@ public class CSVReaderApp {
     private final ApplicationView app;
     public JButton GRPButton;
 
-    Timer timer;
-    AtomicInteger timeSpent = new AtomicInteger();
-
-    TimerTask task = new TimerTask() {
-        public void run() {
-            GRPButton.setText("Time: " + timeSpent.incrementAndGet() + "s");
-        }
-    };
-
-
     public CSVReaderApp(ApplicationView app) {
         this.app = app;
-        timer = new Timer();
+
 
         GRPButton = new JButton("Start GRP");
         GRPButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                timer.scheduleAtFixedRate(task, 0, 1000); // Schedule the task to run every second
 
                 // Create and execute a SwingWorker to run startCSVReader in the background
                 SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
@@ -67,7 +55,6 @@ public class CSVReaderApp {
         String csvFile = DOCS_PATH.toString();
         String line;
         String cvsSplitBy = ","; // Trennzeichen in der CSV-Datei
-        responses.clear(); // Leere die Liste der vorherigen Antworten
 
         // Create a map to group document names by ID
         Map<String, List<String>> idToDocumentNames = new HashMap<>();
@@ -89,13 +76,13 @@ public class CSVReaderApp {
 
             // Iterate through the ID groups and send them to GPT
             for (List<String> documentNames : idToDocumentNames.values()) {
+
                 String prompt = buildPrompt(documentNames);
+                System.out.println("Folgende Anfrage wurde gestellt:\n " + prompt);
 
-                timer.scheduleAtFixedRate(task, 0, 1000); // Schedule the task to run every second
-                String response = String.valueOf(app.manager.callGPT(prompt)); // Aufruf deiner callGPT-Funktion
-                timer.cancel(); // Stop the timer once callGPT is done
-
+                String response = app.manager.callGPT(prompt).orElseThrow();
                 app.chatPane.writeMsg(response, Role.ASSISTANT);
+                System.out.println("Folgende Antwort wurde erhalten:\n " + response);
                 responses.add(response);
 
                 // Schreibe die Response in die GRPS_PATH CSV-Datei
@@ -114,19 +101,7 @@ public class CSVReaderApp {
     }
 
     private String buildPrompt(List<String> documentNames) {
-        StringBuilder promptBuilder = new StringBuilder(instruction);
-        if (!responses.isEmpty()) {
-            promptBuilder
-                    .append("Gruppen:\n")
-                    .append(responses.get(responses.size() - 1))
-                    .append("\n");
-        }
-        promptBuilder
-                .append("Dokumentennamen:\n")
-                .append(String.join("\n", documentNames));
-
-        System.out.println("Folgender Prompt wurde erstellt: " + promptBuilder);
-        return promptBuilder.toString();
+        return instruction + "Dokumentennamen:\n" + String.join("\n", documentNames);
     }
 
 }
